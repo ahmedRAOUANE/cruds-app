4ccd3e2c0c2a20df5ed337e102b3a19e708a50b7
ahmed <122179718+ahmedRAOUANE@users.noreply.github.com>
2023-08-14 18:28:05 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import Navbar from './components/navbar/Navbar';
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import Navbar from "./components/navbar/Navbar";
+
+import { InboxIcon, MailIcon } from "@mui/icons-material";
 
 function App() {
   return (
-      <><Navbar /></>
+    <>
+      <Navbar />
+      <Container>
+        <Grid container>
+          <Grid item>
+            <List>
+              {["Inbox", "Starred", "Send email", "Drafts"].map(
+                (text, index) => (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  </ListItem>
+                )
+              )}
+            </List>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,44 +4,41 @@ import {
   AppBar,
   Box,
   Button,
-  Container,
   IconButton,
   Toolbar,
   Typography,
 } from "@mui/material";
 
-import { MenuIcon } from "@mui/icons-material";
+import { AirSharp } from "@mui/icons-material";
 
 const Navbar = () => {
   return (
-    <Container style={{ maxWidth: "60%", margin: "auto" }}>
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            MUI
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {["Home", "About", "Contact"].map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Container>
+    <AppBar component="nav">
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <AirSharp />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+        >
+          MUI
+        </Typography>
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          {["Home", "About", "Contact"].map((item) => (
+            <Button key={item} sx={{ color: "#fff" }}>
+              {item}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
